eb1c71f2b029156a5d02853c5b2451aa2e690ad1
Frodo <fredymoncada777@gmail.com>
2020-04-06 13:03:08 -0400

    Update FileUtil.java

--- library/src/main/java/cl/tiocomegfas/library/backend/storage/FileUtil.java
+++ library/src/main/java/cl/tiocomegfas/library/backend/storage/FileUtil.java
@@ -6,7 +6,16 @@ import android.graphics.Bitmap;
 import com.snatik.storage.Storable;
 import com.snatik.storage.Storage;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.List;
 
 public class FileUtil {
 
@@ -32,5 +41,34 @@ public class FileUtil {
 
     public static boolean rename(Context context,String path,String newName){
         return new Storage(context).getFile(path).renameTo(new File(newName));
+    }
+
+    public static String readFile(String path) throws IOException{
+
+        File file = new File(path);
+
+        if (file.exists()) {
+            StringBuilder str = new StringBuilder();
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while( (line = br.readLine()) != null ){
+                str.append(line);
+            }
+
+            return str.toString();
+        }
+
+        throw new FileNotFoundException();
+    }
+
+    public static void writeFile(String path, String data) throws IOException{
+
+        File file = new File(path);
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        bw.write(data);
+        bw.flush();
+        bw.close();
     }
 }
